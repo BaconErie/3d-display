c6487fce76454bf3cf68ac55e8cb4818fb6f30f6
BaconErie <baconerie0001@gmail.com>
2025-09-29 13:20:33 -0400

    Finish UI

--- main.cpp
+++ main.cpp
@@ -29,6 +29,46 @@ activate (GtkApplication *app,
   window = GTK_WIDGET (gtk_builder_get_object (builder, "main_window"));
   gtk_window_set_application (GTK_WINDOW (window), app);
 
+  // Get the horizontal_displacement_diagram picture widget and set its file
+  GtkWidget *horizontal_displacement_diagram = GTK_WIDGET (gtk_builder_get_object (builder, "horizontal_displacement_diagram"));
+  if (horizontal_displacement_diagram) {
+    GFile *image_file = g_file_new_for_path("/home/eric/3d-display/programs/program/horizontal-displacement-diagram.png");
+    gtk_picture_set_file(GTK_PICTURE(horizontal_displacement_diagram), image_file);
+    g_object_unref(image_file);
+  } else {
+    g_warning("Could not find diagram picture widget");
+  }
+
+  // Get the vertical_displacement_diagram picture widget and set its file
+  GtkWidget *vertical_displacement_diagram = GTK_WIDGET (gtk_builder_get_object (builder, "vertical_displacement_diagram"));
+  if (vertical_displacement_diagram) {
+    GFile *image_file = g_file_new_for_path("/home/eric/3d-display/programs/program/vertical-displacement-diagram.png");
+    gtk_picture_set_file(GTK_PICTURE(vertical_displacement_diagram), image_file);
+    g_object_unref(image_file);
+  } else {
+    g_warning("Could not find diagram picture widget");
+  }
+
+  // FOV Webcam Image Placeholder
+  GtkWidget *fov_webcam_image = GTK_WIDGET (gtk_builder_get_object (builder, "fov_webcam_image"));
+  if (fov_webcam_image) {
+    GFile *image_file = g_file_new_for_path("/home/eric/3d-display/programs/program/fov-webcam-placeholder.jpg");
+    gtk_picture_set_file(GTK_PICTURE(fov_webcam_image), image_file);
+    g_object_unref(image_file);
+  } else {
+    g_warning("Could not find FOV webcam image widget");
+  }
+
+  // Main Webcam Image Placeholder
+  GtkWidget *main_webcam_image = GTK_WIDGET (gtk_builder_get_object (builder, "main_webcam_image"));
+  if (main_webcam_image) {
+    GFile *image_file = g_file_new_for_path("/home/eric/3d-display/programs/program/fov-webcam-placeholder.jpg");
+    gtk_picture_set_file(GTK_PICTURE(main_webcam_image), image_file);
+    g_object_unref(image_file);
+  } else {
+    g_warning("Could not find Main webcam image widget");
+  }
+
   // Get the CSS provider
   css_provider = gtk_css_provider_new();
   gtk_css_provider_load_from_path(css_provider, "./ui/style.css");
